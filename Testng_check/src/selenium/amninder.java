dbe7e4a1af97760c9d0b5faac7390c37b41cd855
Rohith Reddy <ammysingh1994@gmail.com>
2019-01-31 11:52:05 +0530

    Testng_check

---
+++ Testng_check/src/selenium/amninder.java
@@ -0,0 +1,14 @@
+package selenium;
+
+import org.testng.annotations.Test;
+
+public class amninder {
+	
+	@Test
+	public void ammy()
+	{
+		
+		System.out.println("hello amninder how are you");
+	}
+
+}
